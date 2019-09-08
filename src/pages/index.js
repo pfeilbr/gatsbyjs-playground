5f952d3ac1576cc130975c1524a97e104d9a2772
Brian Pfeil <brian.pfeil@gmail.com>
2019-09-08 18:22:57 -0400

    added feature-01

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Hi people with</h1>
+    <p>feature-01</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
